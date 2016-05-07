d0075eaa309a8248e5e966476cfc433983a829dc
Daniel Santamaria <santaka87@gmail.com>
2016-05-07 09:10:47 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+.checkout
+=========
+
+A Symfony project created on April 30, 2016, 4:03 pm.
---
+++ src/AppBundle/Entity/User.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use STK\MultitenacyBundle\Security\User\MultitenacyUserInterface;
+
+class User implements MultitenacyUserInterface
+{
+
+    /**
+     * @return string
+     */
+    public function getTenant()
+    {
+        return 'tenant';
+    }
+
+    /**
+     * Returns the roles granted to the user.
+     *
+     * <code>
+     * public function getRoles()
+     * {
+     *     return array('ROLE_USER');
+     * }
+     * </code>
+     *
+     * Alternatively, the roles might be stored on a ``roles`` property,
+     * and populated in any number of different ways when the user object
+     * is created.
+     *
+     * @return (Role|string)[] The user roles
+     */
+    public function getRoles()
+    {
+        return ['ROLE_USER'];
+    }
+
+    /**
+     * Returns the password used to authenticate the user.
+     *
+     * This should be the encoded password. On authentication, a plain-text
+     * password will be salted, encoded, and then compared to this value.
+     *
+     * @return string The password
+     */
+    public function getPassword()
+    {
+        return '12345678';
+    }
+
+    /**
+     * Returns the salt that was originally used to encode the password.
+     *
+     * This can return null if the password was not encoded using a salt.
+     *
+     * @return string|null The salt
+     */
+    public function getSalt()
+    {
+        return null;
+    }
+
+    /**
+     * Returns the username used to authenticate the user.
+     *
+     * @return string The username
+     */
+    public function getUsername()
+    {
+        return 'santaka';
+    }
+
+    /**
+     * Removes sensitive data from the user.
+     *
+     * This is important if, at any given point, sensitive information like
+     * the plain-text password is stored on this object.
+     */
+    public function eraseCredentials()
+    {
+    }
+}
